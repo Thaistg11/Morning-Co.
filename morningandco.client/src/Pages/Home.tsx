e321ef09e4477c07250c712186f0f07e7da3f24a
TG <thais@tguide.com.br>
2025-04-16 17:02:36 +0100

    Add Bootstrap

--- morningandco.client/src/Pages/Home.tsx
+++ morningandco.client/src/Pages/Home.tsx
@@ -7,7 +7,18 @@ function Home() {
         <AuthorizeView>
             <span><LogoutLink>Logout <AuthorizedUser value="email" /></LogoutLink></span>
             <WeatherForecast />
+            <button type="button" className="btn btn-primary">Primary</button>
+            <button type="button" className="btn btn-secondary">Secondary</button>
+            <button type="button" className="btn btn-success">Success</button>
+            <button type="button" className="btn btn-danger">Danger</button>
+            <button type="button" className="btn btn-warning">Warning</button>
+            <button type="button" className="btn btn-info">Info</button>
+            <button type="button" className="btn btn-light">Light</button>
+            <button type="button" className="btn btn-dark">Dark</button>
+
         </AuthorizeView>
+
+
     );
 }
 
