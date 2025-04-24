e0543fd59973381d998cfc7be8e644899a974aaa
TG <thais@tguide.com.br>
2025-04-24 16:18:04 +0100

    Add photo gallery to the home page

--- morningandco.client/src/Components/Footer.tsx
+++ morningandco.client/src/Components/Footer.tsx
@@ -2,27 +2,28 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="text-center text-white" style={{ backgroundColor: "rgba(10, 10, 10, 1)" }}>
+        <footer className="custom-Footer text-center text-white" style={{ backgroundColor: "rgba(10, 10, 10, 1)" }}>
             <div className="container pt-4">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
                 <section className="mb-4">
                     <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button">
-                        <a href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F" className="fa fa-facebook"></a>
+                        <a href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F" className="fa fa-facebook" target="_blank"
+                            rel="noopener noreferrer" ></a>
                     </a>
 
                     <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button">
-                        <a href="https://www.google.co.uk/maps/" className="fa fa-google"></a>
+                        <a href="https://www.google.co.uk/maps/" className="fa fa-google" target="_blank"
+                            rel="noopener noreferrer"></a>
                     </a>
 
                     <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button">
-                        <a href="https://www.instagram.com/" className="fa fa-instagram"></a>
+                        <a href="https://www.instagram.com/ " className="fa fa-instagram" target="_blank"
+                            rel="noopener noreferrer"></a>
                     </a>
                 </section>
             </div>
 
-            <div >
-                <h4 className="footer.Font">Morning & Co</h4>
-            </div>
+            
         </footer>
     );
 };
--- morningandco.client/src/Components/HomeContainer.tsx
+++ morningandco.client/src/Components/HomeContainer.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import CoffeeEditImg from '../Img/CoffeeEdit.jpg';
-import FocacciaEditImg from '../Img/FocacciaEdit.jpg';
+import CoffeeImg from '../Img/Coffee.jpg';
+import FocacciaImg from '../Img/Focaccia.jpg';
+import EggsBeneditImg from '../Img/EggsBenedit.jpg';
+import FacadeImg from '../Img/Facade.png';
+import OurSpaceImg from '../Img/OurSpace.jpg';
+import SanduichImg from '../Img/Sanduich.jpg';
+import CellingImg from '../Img/Celling.jpg';
+import BrunchImg from '../Img/Brunch.jpg';
+import PancakeImg from '../Img/Pancake.jpg';
+
 
 const HomeContainer = () => {
     return (
@@ -14,9 +22,9 @@ const HomeContainer = () => {
                 </li>
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                            <img className="d-block w-100" src={FocacciaEditImg}  alt="First slide" />
+                            <img className="d-block w-100" src={FocacciaImg}  alt="First slide" />
                         <div className="carousel-caption">
-                            <h1 className="h1">Morning&Co</h1>
+                                <h1 className="h1">Welcome to Your Favorite Coffee Spot</h1>
                                 <h2 className="h2">Where comfort meets coffee</h2>
                             <div className="row justify-content-center">
                                 <div className="col">
@@ -27,7 +35,7 @@ const HomeContainer = () => {
                 </div>
                 
                 <div className="carousel-item">
-                            <img className="d-block w-100" src={CoffeeEditImg} alt="Second slide" />
+                            <img className="d-block w-100" src={CoffeeImg} alt="Second slide" />
                             <div className="carousel-caption">
                                 <h2 className="h1">Come for the coffee, stay for the brunch</h2>
                                 <div className="row justify-content-center">
@@ -56,16 +64,11 @@ const HomeContainer = () => {
                     <div className="row mt-5 mx-1 align-items-center">
                         <div className="col-12 col-lg-8">
                             <h2>About Us </h2>
-                            <p className="text-justify">At Morning&Co, we believe the best part of the day deserves something truly special. </p>
-                            <p className="text-justify">We are more than just a coffee shop, we are a space where craft meets comfort,
-                                and where every cup and every dish is made with purpose. Our signature coffees are brewed from carefully selected,
-                                high-quality beans, offering a rich and memorable experience in every sip.</p>
-                            <p className="text-justify">Our brunch menu, led by a chef with training from the world-renowned Le Cordon Bleu,
-                                brings together culinary expertise and feel-good flavors.
-                                From fresh pastries to hearty plates, we blend elegance and coziness into every bite.</p>
-                            <p className="text-justify">  <p className="text-justify">Our brunch menu, led by a chef with training from the world-renowned Le Cordon Bleu,
-                                brings together culinary expertise and feel-good flavors.
-                                From fresh pastries to hearty plates, we blend elegance and coziness into every bite.</p></p>
+                            <p className="text-justify">Morning&Co is more than just a coffee shop, it's a space where craft and comfort come together.
+                                We offer signature coffees brewed from high-quality beans, providing a rich, memorable experience in every sip. </p>
+                            <p className="text-justify">Our brunch menu, crafted by a Le Cordon Bleu-trained chef, blends culinary expertise with feel-good flavors,
+                                offering everything from fresh pastries to hearty dishes, blending elegance with coziness in every bite.</p>
+                           
                         </div>
                         <div className="d-none d-lg-block col-lg-4">
                             {/*<img src={ } className="img-fluid" alt="About us image">*/}
@@ -75,6 +78,35 @@ const HomeContainer = () => {
 
             </section>
             {/* End About Us*/}
+
+
+            {/* Start Photo Gallery */}
+            <section className="photo-gallery container mt-5">
+                <h2 className="mb-4 text-center">Gallery</h2>
+                <div className="row">
+                    <div className="column col-12 col-sm-6 col-md-3">
+                        <img src={CellingImg} style={{ width: "100%" }} alt="Celling" />
+                        <img src={BrunchImg} style={{ width: "100%" }} alt="Brunch" />
+
+                       
+                    </div>
+                    <div className="column col-12 col-sm-6 col-md-3">
+                        <img src={BrunchImg} style={{ width: "100%" }} alt="Brunch" />
+                        <img src={PancakeImg} style={{ width: "100%" }} alt="Pancake" />
+                      
+                    </div>
+                    <div className="column col-12 col-sm-6 col-md-3">
+                        <img src={EggsBeneditImg} style={{ width: "100%" }} alt="Eggs Benedit" />
+                        <img src={SanduichImg} style={{ width: "100%" }} alt="Sanduich" />
+                    </div>
+                    <div className="column col-12 col-sm-6 col-md-3">
+                        <img src={FacadeImg} style={{ width: "100%" }} alt="Facade" />
+                        <img src={OurSpaceImg} style={{ width: "100%" }} alt="Our Space" />
+                    </div>
+                </div>
+            </section>
+            {/* End Photo Gallery */}
+
 
         </div>
     );
